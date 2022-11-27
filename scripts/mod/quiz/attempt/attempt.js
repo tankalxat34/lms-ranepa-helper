05946c7a0ad39e96be092992c2199587cf89d174
Alexander Podstrechnyy <tankalxat34@gmail.com>
2022-11-27 22:13:44 +0300

    Добавлена функция получения списка вопросов теста

--- scripts/mod/quiz/attempt/attempt.js
+++ scripts/mod/quiz/attempt/attempt.js
@@ -28,34 +28,87 @@ const INTERNET_PATTERNS = {
     }
 }
 
+const USER = {
+    user_agent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36"
+}
 
-function fillButtons(qtext = "qtext") {
-    let questionNodes = document.getElementsByClassName(qtext)
-    let answerNodes = document.getElementsByClassName("answer")
+
+function getQuestions(qtext) {
+    /* 
+        Возвращает список всех вопросов на странице
+    */
+    
+    let questionNodes = document.getElementsByClassName(qtext);
+    let answersArray = new Array();
+
+    for (let i = 0; i < questionNodes.length; i++) {
+        let questionText = questionNodes[i].innerText;
+        if (questionNodes[i].childNodes[0].nodeName !== "#text") {
+            questionText = questionNodes[i].childNodes[0].innerText;
+        }
+        answersArray.push(questionText);
+    }
+
+    return Object.assign(answersArray)
+}
+
+
+
+function fillButtons(qtext="qtext") {
+    /* 
+        Заполняет блоки с вопросами кнопками с поисковыми системами 
+    */
+
+    let questionTexts = getQuestions(qtext);
+    
+    let answerNodes = document.getElementsByClassName("answer");
 
     var questionIndex = 0
     for (let answerNode of answerNodes) {
 
-        console.log(questionNodes[questionIndex].childNodes)
+        let answerButtonDiv = document.createElement("div");
 
-        var questionText = questionNodes[questionIndex].innerText
-
-        if (questionNodes[questionIndex].childNodes[0].nodeName !== "#text") {
-            questionText = questionNodes[questionIndex].childNodes[0].innerText
-        } 
+        var questionText = questionTexts[questionIndex];
 
         for (let system of Object.keys(INTERNET_PATTERNS)) {
             
-            let button = document.createElement("a")
-            button.classList = "btn btn-secondary"
-            button.style.margin = "0 5px"
-            button.href = INTERNET_PATTERNS[system].url + questionText.replaceAll(" ", INTERNET_PATTERNS[system].replacedSpace)
-            button.target = "_blank"
-            button.innerText = INTERNET_PATTERNS[system].name
-            answerNode.appendChild(button)
+            let button = document.createElement("a");
+            button.classList = "btn btn-secondary";
+            button.style.margin = "0 5px";
+            button.target = "_blank";
+
+            button.href = INTERNET_PATTERNS[system].url + questionText.replaceAll(" ", INTERNET_PATTERNS[system].replacedSpace);
+            button.innerText = INTERNET_PATTERNS[system].name;
+
+            answerButtonDiv.appendChild(button);
         }
-        questionIndex++
+        answerNode.appendChild(answerButtonDiv);
+
+        questionIndex++;
     }
 }
 
-fillButtons()
+
+
+function addQuickAnswer(qtext="qtext") {
+    /* 
+        Добавляет текст быстрого ответа из Яндекса в каждый вопрос теста
+    */
+    
+    let questionTexts = getQuestions(qtext);
+}
+
+
+
+function addMainBlock() {
+    /*
+        Добавляет блок для управления тестом
+        - поделиться ответами
+        - заполнить ответами
+        - 
+    */
+    
+    return 0;
+}
+
+fillButtons();
