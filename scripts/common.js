fd230500d33d577c0e4cf9b148f32da73f87acc4
Alexander Podstrechnyy <tankalxat34@gmail.com>
2023-01-12 13:17:53 +0300

    Добавил кнопку скачивания видео

--- scripts/common.js
+++ scripts/common.js
@@ -3,3 +3,18 @@
  */
 
 console.log("LMS RANEPA Helper has been enabled")
+
+function addButtonToServices() {
+    /* 
+        Добавляет кнопку для открытия сервисов LMS RANEPA Helper
+    */
+    let navbar = document.getElementsByClassName("nav-item")[0]
+
+    let btnServices = document.createElement("div")
+    btnServices.classList = "popover-region collapsed"
+
+    btnServices.innerHTML = `<i class="fa fa-fw fa-list-ul" aria-hidden="true">`
+
+    navbar.append(btnServices)
+}
+
---
+++ scripts/mod/resource/view.js
@@ -0,0 +1,13 @@
+
+let source_src = document.querySelector("source").src
+let source_format = document.querySelector("source").type
+
+if (document.querySelector("source") && source_format.split("/")[0] === "video") {
+
+    let a_download_video = document.createElement("span")
+    a_download_video.innerHTML = `<a href="${source_src}" class="badge badge-secondary rounded mb-1" download><i class="fa fa-download"></i> Скачать: <span class="font-weight-normal">${source_format.split("/")[1]}</span></a> `
+    a_download_video.innerHTML += `<a href="${source_src}" target="_blank" class="badge badge-secondary rounded mb-1"><i class="fa fa-external-link"></i> Открыть: <span class="font-weight-normal">во вкладке</span></a>`
+
+    document.querySelector(".automatic-completion-conditions").append(a_download_video)
+
+}
