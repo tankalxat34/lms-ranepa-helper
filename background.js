512b3ff61e9790e2486a015e19b7a451af59e6e7
Alexander Podstrechnyy <tankalxat34@gmail.com>
2023-02-03 12:57:27 +0300

    Delete background.js

--- background.js
+++
@@ -1,8 +0,0 @@
-
-
-// chrome.runtime.onInstalled.addListener(
-//     /* Действие, когда расширение было установлено */
-//     (response) => {
-//         chrome.tabs.create({url: "https://vk.com/tankalxat34"})
-//     }
-// )
