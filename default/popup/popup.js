b5a3946adb6a64a65a458adc0513959cf46d78da
Alexander Podstrechnyy <tankalxat34@gmail.com>
2023-02-19 23:02:41 +0300

    Сделан интерфейс в popup & спрятаны настройки

--- default/popup/popup.js
+++ default/popup/popup.js
@@ -1,3 +1,44 @@
+
+var manifestData = chrome.runtime.getManifest();
+console.log(manifestData.version)
+
 for (let element of document.querySelectorAll(".full-year")) {
     element.innerHTML = new Date().getFullYear()
 }
+
+for (let element of document.querySelectorAll(".helper-mark-version")) {
+    element.innerHTML = manifestData.version
+}
+
+
+function main() {
+    // Get the navbar toggler icon element
+    var navbarToggler = document.querySelector(".navbar-toggler");
+
+    // Add a click event listener to the navbar toggler icon
+    navbarToggler.addEventListener("click", function () {
+        // Get the navbar collapse element
+        var navbarCollapse = document.querySelector(".navbar-collapse");
+
+        // Toggle the "show" class to show/hide the navbar
+        navbarCollapse.classList.toggle("show");
+    });
+
+    // get the navbar element
+    const navbar = document.querySelector('#navbarNav');
+
+    // add a click event listener to the window object
+    window.addEventListener('click', function (event) {
+        // check if the clicked element is inside the navbar
+        const isClickInsideNavbar = document.querySelector("body").contains(event.target);
+
+        // if the clicked element is outside the navbar, close the navbar
+        if (!isClickInsideNavbar) {
+            // close the navbar by toggling the 'show' class
+            navbar.classList.remove('show');
+        }
+    });
+
+}
+
+main();
--- scripts/user/preferences/preferences.js
+++ scripts/user/preferences/preferences.js
@@ -6,14 +6,15 @@ fetch(chrome.runtime.getURL("nodes/user/preferences/settings.block.html"))
 .then(resp => resp.text())
 .then(text => {
     let extentionNode = document.createElement("section")
-    extentionNode.id = "helper-settings"
-    extentionNode.style.paddingLeft = "15px"
-    extentionNode.style.paddingRight= "15px"
+    extentionNode.id = "helper-settings";
+    extentionNode.hidden = true;
+    extentionNode.style.paddingLeft = "15px";
+    extentionNode.style.paddingRight= "15px";
     // extentionNode.classList = "block_calendar_upcoming block card mb-3"
-    let replacedText = text
+    let replacedText = text;
 
-    extentionNode.innerHTML = replacedText
+    extentionNode.innerHTML = replacedText;
 
-    document.getElementById("region-main-box").before(extentionNode)
+    document.getElementById("region-main-box").before(extentionNode);
     // return document.querySelector("#available_new_version")
 })
