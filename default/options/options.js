8990f66e78aba9f16e38056c5ed5933a994ab5b9
Alexander Podstrechnyy <tankalxat34@gmail.com>
2023-02-20 22:02:58 +0300

    Автоматический вариант настроек

--- default/options/options.js
+++ default/options/options.js
@@ -6,7 +6,28 @@ window.onselectstart = () => false;
  * Вернуть массив с выбранными селекторами настроек
  */
 function getOptionFields() {
-    return document.querySelectorAll("#helper-settings-card input")
+    return document.querySelectorAll("#helper-settings-card input");
+}
+
+
+/**
+ * Создает объект с ключами-id и значениями value или cheched
+ */
+function generateOptionsObj() {
+    let formElements = getOptionFields();
+    let formData = {};
+
+    for (let i = 0; i < formElements.length; i++) {
+        const element = formElements[i];
+        if (element.id) {
+            if (element.type === "checkbox") {
+                formData[element.id] = element.checked;
+            } else {
+                formData[element.id] = element.value;
+            }
+        }
+    }
+    return formData;
 }
 
 
@@ -14,61 +35,38 @@ function getOptionFields() {
  * Сохранить настройки в память Chrome
 */
 function saveOptions() {
-    let doc_newest_commit_message = document.querySelector("#helper-settings-get_newest_commit_message").checked;
-    let doc_show_clck = document.querySelector("#helper-settings-show_clck").checked;
-    let doc_show_rugost = document.querySelector("#helper-settings-show_rugost").checked;
-    let doc_show_searchinput_courses = document.querySelector("#helper-settings-show_searchinput_courses").checked;
-    let doc_disable_yametrika = document.querySelector("#helper-settings-disable_yametrika").checked;
-    let doc_changeable_form_action = document.querySelector("#helper-settings-changeable_form_action").checked;
-    let doc_show_hidden_inputs = document.querySelector("#helper-settings-show_hidden_inputs").checked;
-    
-    chrome.storage.sync.set({
-        doc_newest_commit_message,
-        doc_show_clck,
-        doc_show_rugost,
-        doc_show_searchinput_courses,
-        doc_disable_yametrika,
-        doc_changeable_form_action,
-        doc_show_hidden_inputs
-    }, (e) => {
+
+    let formData = generateOptionsObj();
+    console.log(formData)
+
+    chrome.storage.sync.set(formData, (e) => {
         console.log(e);
         console.log('Options saved');
     });
 }
 
+
 /**
  * Загрузить из памяти Chrome значения опций
  */
 function loadOptions() {
-    chrome.storage.sync.get([
-        "doc_newest_commit_message",
-        "doc_show_clck",
-        "doc_show_rugost",
-        "doc_show_searchinput_courses",
-        "doc_disable_yametrika",
-        "doc_changeable_form_action",
-        "doc_show_hidden_inputs"
-    ], (options) => {
-        
-        document.querySelector("#helper-settings-get_newest_commit_message").checked = options.doc_newest_commit_message;
-        document.querySelector("#helper-settings-show_clck").checked = options.doc_show_clck;
-        document.querySelector("#helper-settings-show_rugost").checked = options.doc_show_rugost;
-        document.querySelector("#helper-settings-show_searchinput_courses").checked = options.doc_show_searchinput_courses;
-        document.querySelector("#helper-settings-disable_yametrika").checked = options.doc_disable_yametrika;
-        document.querySelector("#helper-settings-changeable_form_action").checked = options.doc_changeable_form_action;
-        document.querySelector("#helper-settings-show_hidden_inputs").checked = options.doc_show_hidden_inputs;
 
+    let formData = generateOptionsObj();
+    let selectors = Object.keys(formData);
 
-        console.log("doc_newest_commit_message", options.doc_newest_commit_message)
-        console.log("doc_show_clck", options.doc_show_clck)
-        console.log("doc_show_rugost", options.doc_show_rugost)
-        console.log("doc_show_searchinput_courses", options.doc_show_searchinput_courses)
-        console.log("doc_disable_yametrika", options.doc_disable_yametrika)
-        console.log("doc_changeable_form_action", options.doc_changeable_form_action)
-        console.log("doc_show_hidden_inputs", options.doc_show_hidden_inputs)
+    chrome.storage.sync.get(selectors, (options) => {
+
+        for (let index = 0; index < selectors.length; index++) {
+            const s = selectors[index];
+            if (document.querySelector(`#${s}`).type === "checkbox") {
+                document.querySelector(`#${s}`).checked = options[s];
+            } else {
+                document.querySelector(`#${s}`).value = options[s];
+            }
+
+        }
     });
 }
 
-// document.querySelector("#helper-settings-btn_save").addEventListener("click", saveOptions)
 document.querySelector("#helper-settings-card").addEventListener("click", saveOptions)
 window.addEventListener("load", loadOptions)
