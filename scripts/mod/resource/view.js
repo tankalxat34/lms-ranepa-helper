f855864ef74b61b8ddeab17e9d0d3d6444dc4fc9
Alexander Podstrechnyy <tankalxat34@gmail.com>
2023-02-26 12:00:58 +0300

    фикс настройки с видео

--- scripts/mod/resource/view.js
+++ scripts/mod/resource/view.js
@@ -12,18 +12,16 @@ chrome.storage.sync.get(["_option_names_array"], (options) => {
                 videoElements.forEach(element => element.removeAttribute("title"));
             }
             
-            if (options['helper-settings-show_download_video_button']) {
-                let source_src = document.querySelector("source").src
-                let source_format = document.querySelector("source").type
-                
-                if (document.querySelector("source") && source_format.split("/")[0] === "video") {
-                
-                    let a_download_video = document.createElement("span")
-                    a_download_video.innerHTML = `<a href="${source_src}" class="badge badge-info rounded mb-1" download><i class="fa fa-download"></i> Скачать: <span class="font-weight-normal">${source_format.split("/")[1]}</span></a> `
-                    if (options['helper-settings-show_targetblank_video_button']) a_download_video.innerHTML += `<a href="${source_src}" target="_blank" class="badge badge-info rounded mb-1"><i class="fa fa-external-link"></i> Открыть: <span class="font-weight-normal">во вкладке</span></a>`
-                
-                    document.querySelector(".automatic-completion-conditions").append(a_download_video)
-                }
+            let source_src = document.querySelector("source").src
+            let source_format = document.querySelector("source").type
+            
+            if (document.querySelector("source") && source_format.split("/")[0] === "video") {
+            
+                let a_download_video = document.createElement("span")
+                if (options['helper-settings-show_download_video_button']) a_download_video.innerHTML = `<a href="${source_src}" class="badge badge-info rounded mb-1" download><i class="fa fa-download"></i> Скачать: <span class="font-weight-normal">${source_format.split("/")[1]}</span></a> `
+                if (options['helper-settings-show_targetblank_video_button']) a_download_video.innerHTML += `<a href="${source_src}" target="_blank" class="badge badge-info rounded mb-1"><i class="fa fa-external-link"></i> Открыть: <span class="font-weight-normal">во вкладке</span></a>`
+            
+                document.querySelector(".automatic-completion-conditions").append(a_download_video)
             }
         }
     });
