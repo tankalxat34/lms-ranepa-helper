2493f7b6d0acc5086414bcee970eae3af6f39235
Alexander Podstrechnyy <tankalxat34@gmail.com>
2023-04-05 17:26:41 +0300

    Фикс работы с видео

--- scripts/mod/resource/view.js
+++ scripts/mod/resource/view.js
@@ -11,17 +11,12 @@ chrome.storage.sync.get(["_option_names_array"], (options) => {
                 
                 videoElements.forEach(element => element.removeAttribute("title"));
             }
-            
-            if (document.querySelector("source")) {
-                let source_src = document.querySelector("source").src
-                let source_format = document.querySelector("source").type
-            }
 
-            if (document.querySelector("source") && source_format.split("/")[0] === "video") {
+            if (document.querySelector("source") && document.querySelector("source").type.split("/")[0] === "video") {
             
                 let a_download_video = document.createElement("span")
-                if (options['helper-settings-show_download_video_button']) a_download_video.innerHTML = `<a href="${source_src}" class="badge badge-info rounded mb-1" download><i class="fa fa-download"></i> Скачать: <span class="font-weight-normal">${source_format.split("/")[1]}</span></a> `
-                if (options['helper-settings-show_targetblank_video_button']) a_download_video.innerHTML += `<a href="${source_src}" target="_blank" class="badge badge-info rounded mb-1"><i class="fa fa-external-link"></i> Открыть: <span class="font-weight-normal">во вкладке</span></a>`
+                if (options['helper-settings-show_download_video_button']) a_download_video.innerHTML = `<a href="${document.querySelector("source").src}" class="badge badge-info rounded mb-1" download><i class="fa fa-download"></i> Скачать: <span class="font-weight-normal">${document.querySelector("source").src.split(".")[document.querySelector("source").src.split(".").length - 1]}</span></a> `
+                if (options['helper-settings-show_targetblank_video_button']) a_download_video.innerHTML += `<a href="${document.querySelector("source").src}" target="_blank" class="badge badge-info rounded mb-1"><i class="fa fa-external-link"></i> Открыть: <span class="font-weight-normal">во вкладке</span></a>`
             
                 document.querySelector(".automatic-completion-conditions").append(a_download_video)
             }
