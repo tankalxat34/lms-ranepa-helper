4b32e4fa3568a159c91976219860373019968937
Alexander Podstrechnyy <tankalxat34@gmail.com>
2023-10-13 22:02:59 +0300

    Добавлено контекстное меню
    - копирование ссылки на страницу
    - копирование ссылки на страницу в формате ГОСТ (RUS & ENG)
    - открытие иконки сайта
    - копирование заголовка страницы

---
+++ scripts/contextMenu.js
@@ -0,0 +1,40 @@
+function copyToClipboard(data) {
+    // https://stackoverflow.com/questions/3436102/copy-to-clipboard-in-chrome-extension
+    const copySource = document.createElement('textarea');
+    copySource.textContent = data;
+    document.body.appendChild(copySource);
+    copySource.select();
+    document.execCommand('copy');
+    document.body.removeChild(copySource);
+}
+
+
+chrome.runtime.onMessage.addListener(function (request) {
+    let resp = request.currentTab;
+    let date = new Date();
+
+    switch (request.action) {
+        case 'copyToClipboardTitle':
+            copyToClipboard(request.currentTab.title);
+            break;
+
+        case 'copyToClipboardURL':
+            copyToClipboard(request.currentTab.url);
+            break;
+
+        case 'copyToClipboard-openIcon':
+            window.open(resp.favIconUrl, "_blank");
+            break;
+
+        case 'copyToClipboardGOST-4':
+            copyToClipboard(`${resp.title}. — Текст : электронный // ${new URL(resp.url).host} : [сайт]. — URL: ${resp.url} (дата обращения: ${date.toLocaleDateString()}).`);
+            break;
+
+        case 'copyToClipboardGOST-EN':
+            copyToClipboard(`${resp.title}, ${resp.url} (accessed: ${new Date().toLocaleDateString('en-us', { year:"numeric", month:"long", day:"numeric"})}).`);
+            break;
+
+        default:
+            break;
+    }
+})
